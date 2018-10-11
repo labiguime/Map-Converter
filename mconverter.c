6e8daba5abfc967f8a7fed6227a8d9347a43ac84
Leo <leo_abiguime@sfu.ca>
2018-10-11 16:41:25 -0700

    Fix coding style

--- mconverter.c
+++ mconverter.c
@@ -1,31 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    char input_name[ 64 ] ;
-    memset(&input_name[0], 0, sizeof(input_name));
-    char output_name[ 64 ] ;
-    memset(&output_name[0], 0, sizeof(output_name));
-    char json[6] = ".json" ;
+    char input_name[64];
+    char output_name[64];
+    char json[6] = ".json";
     char str[1024]; // maximum size of a line
-    int firstplacement = 0 ;
-    int InPlacement ; // are we within <Placement> ?
-    FILE *original ;
-    FILE *converted ;
 
+    int firstplacement = 0;
+    int InPlacement; // are we within <Placement> ?
 
-    printf ("\nEnter the name of your XML file: ") ;
-    scanf ("%s", input_name);
+    FILE *original;
+    FILE *converted;
+
+    memset(&input_name[0], 0, sizeof(input_name));
+    memset(&output_name[0], 0, sizeof(output_name));
+
+    printf("\nEnter the name of your XML file: ");
+    scanf("%s", input_name);
     printf("\n");
-    if( strstr (input_name, ".xml") == NULL ) {// the user didn't add .xml at the end of the file name
-        strcat (input_name, ".xml") ; // so we add it ourselves
-    }
-    strncpy (output_name, input_name, strlen(input_name)-4);
-    strcat (output_name, json) ;
 
-    original  = fopen(input_name , "r"); // now we read the file
-    if(original == NULL) {
+    if (strstr(input_name, ".xml") == NULL) {// the user didn't add .xml at the end of the file name
+        strcat(input_name, ".xml"); // so we add it ourselves
+    }
+    strncpy(output_name, input_name, strlen(input_name)-4);
+    strcat(output_name, json);
+
+    original = fopen(input_name , "r"); // now we read the file
+    if (original == NULL) {
       perror("Error opening file");
       return 0;
     }
@@ -34,20 +38,18 @@ int main()
     fprintf( converted, "%s", "[\n");
     while(!feof(original))
     {
-        if( fgets(str, 1024, original) != NULL ) {
-           if( strstr( str, "<Placement>") != NULL ) {
-                if(firstplacement == 0) {
+        if (fgets(str, 1024, original) != NULL) {
+           if (strstr(str, "<Placement>") != NULL) {
+                if (firstplacement == 0) {
                     firstplacement = 1;
-                    fprintf( converted, "%s", " {\n");
+                    fprintf(converted, "%s", " {\n");
                 }
-                else{
-                   fprintf( converted, "%s", ",\n {\n");
+                else {
+                   fprintf(converted, "%s", ",\n {\n");
                 }
-
-
-               InPlacement = 1 ;
+                InPlacement = 1 ;
            }
-           else if( strstr( str, "<HashName>") != NULL && InPlacement == 1 ) {
+           else if (strstr(str, "<HashName>") != NULL && InPlacement == 1) {
                 char *token;
                 token = strtok(str, ">");
                 token = strtok(NULL, ">");
@@ -55,11 +57,11 @@ int main()
                 memset(&copy[0], 0, sizeof(copy));
                 char copy1[256] = "  \"model\": ";
                 strncpy(copy, token, strlen(token)-10);
-                strcat( copy1, copy ) ;
-                strcat( copy1, ",\n") ;
-                fprintf( converted, "%s", copy1);
+                strcat(copy1, copy ) ;
+                strcat(copy1, ",\n") ;
+                fprintf(converted, "%s", copy1);
            }
-           else if( strstr( str, "<X>") != NULL && InPlacement == 1 ) {
+           else if (strstr(str, "<X>") != NULL && InPlacement == 1) {
                 char *token;
                 token = strtok(str, ">");
                 token = strtok(NULL, ">");
@@ -67,11 +69,11 @@ int main()
                 memset(&copy[0], 0, sizeof(copy));
                 char copy1[256] = "  \"x\": ";
                 strncpy(copy, token, strlen(token)-3);
-                strcat( copy1, copy ) ;
-                strcat( copy1, ",\n") ;
-                fprintf( converted, "%s", copy1);
+                strcat(copy1, copy ) ;
+                strcat(copy1, ",\n") ;
+                fprintf(converted, "%s", copy1);
            }
-           else if( strstr( str, "<Y>") != NULL && InPlacement == 1 ) {
+           else if (strstr(str, "<Y>") != NULL && InPlacement == 1) {
                 char *token;
                 token = strtok(str, ">");
                 token = strtok(NULL, ">");
@@ -79,11 +81,11 @@ int main()
                 memset(&copy[0], 0, sizeof(copy));
                 char copy1[256] = "  \"y\": ";
                 strncpy(copy, token, strlen(token)-3);
-                strcat( copy1, copy ) ;
-                strcat( copy1, ",\n") ;
-                fprintf( converted, "%s", copy1);
+                strcat(copy1, copy ) ;
+                strcat(copy1, ",\n") ;
+                fprintf(converted, "%s", copy1);
            }
-           else if( strstr( str, "<Z>") != NULL && InPlacement == 1 ) {
+           else if (strstr(str, "<Z>") != NULL && InPlacement == 1) {
                 char *token;
                 token = strtok(str, ">");
                 token = strtok(NULL, ">");
@@ -91,11 +93,11 @@ int main()
                 memset(&copy[0], 0, sizeof(copy));
                 char copy1[256] = "  \"z\": ";
                 strncpy(copy, token, strlen(token)-3);
-                strcat( copy1, copy ) ;
-                strcat( copy1, ",\n") ;
-                fprintf( converted, "%s", copy1);
+                strcat(copy1, copy ) ;
+                strcat(copy1, ",\n") ;
+                fprintf(converted, "%s", copy1);
            }
-           else if( strstr( str, "<Pitch>") != NULL && InPlacement == 1 ) {
+           else if (strstr(str, "<Pitch>") != NULL && InPlacement == 1) {
                 char *token;
                 token = strtok(str, ">");
                 token = strtok(NULL, ">");
@@ -103,11 +105,11 @@ int main()
                 memset(&copy[0], 0, sizeof(copy));
                 char copy1[256] = "  \"rx\": ";
                 strncpy(copy, token, strlen(token)-7);
-                strcat( copy1, copy ) ;
-                strcat( copy1, ",\n") ;
-                fprintf( converted, "%s", copy1);
+                strcat(copy1, copy ) ;
+                strcat(copy1, ",\n") ;
+                fprintf(converted, "%s", copy1);
            }
-           else if( strstr( str, "<Roll>") != NULL && InPlacement == 1 ) {
+           else if (strstr(str, "<Roll>") != NULL && InPlacement == 1) {
                 char *token;
                 token = strtok(str, ">");
                 token = strtok(NULL, ">");
@@ -115,11 +117,11 @@ int main()
                 memset(&copy[0], 0, sizeof(copy));
                 char copy1[256] = "  \"ry\": ";
                 strncpy(copy, token, strlen(token)-6);
-                strcat( copy1, copy ) ;
-                strcat( copy1, ",\n") ;
-                fprintf( converted, "%s", copy1);
+                strcat(copy1, copy ) ;
+                strcat(copy1, ",\n") ;
+                fprintf(converted, "%s", copy1);
            }
-           else if( strstr( str, "<Yaw>") != NULL && 1 == InPlacement ) {
+           else if (strstr(str, "<Yaw>") != NULL && 1 == InPlacement) {
                 char *token;
                 token = strtok(str, ">");
                 token = strtok(NULL, ">");
@@ -127,13 +129,13 @@ int main()
                 memset(&copy[0], 0, sizeof(copy));
                 char copy1[256] = "  \"rz\": ";
                 strncpy(copy, token, strlen(token)-5);
-                strcat( copy1, copy ) ;
-                strcat( copy1, ",\n") ;
-                fprintf( converted, "%s", copy1);
-                fprintf( converted, "%s", "  \"col\": true,\n  \"freeze\": true\n");
+                strcat(copy1, copy ) ;
+                strcat(copy1, ",\n") ;
+                fprintf(converted, "%s", copy1);
+                fprintf(converted, "%s", "  \"col\": true,\n  \"freeze\": true\n");
            }
-           else if( strstr( str, "</Placement>") != NULL ) {
-               fprintf( converted, "%s", " }");
+           else if (strstr( str, "</Placement>") != NULL) {
+               fprintf(converted, "%s", " }");
                InPlacement = 0 ;
            }
         }
